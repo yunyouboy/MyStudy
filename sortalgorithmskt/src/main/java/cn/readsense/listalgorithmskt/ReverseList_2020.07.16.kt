a1e0e18d8025d10e68eb64c6e4fba3731320b16d
Qyg <qyg712314979@163.com>
2020-07-16 18:44:01 +0800

    1.每日算法提交

---
+++ sortalgorithmskt/src/main/java/cn/readsense/listalgorithmskt/ReverseList_2020.07.16.kt
@@ -0,0 +1,36 @@
+package cn.readsense.listalgorithmskt
+
+/**
+ *Author:qyg
+ *DATE:2020/7/16 18:33
+ *Description：
+ **/
+
+fun main() {
+
+    var header: ListNode? = null
+    header = ListNode(0)
+    header?.next = ListNode(1)
+    header?.next?.next = ListNode(2)
+    header?.next?.next?.next = ListNode(3)
+    header?.next?.next?.next?.next = ListNode(4)
+    var reverseList = reverseList(header)
+    while (null != reverseList) {
+        println(reverseList.`val`)
+        reverseList = reverseList.next
+    }
+}
+
+private fun reverseList(header: ListNode?): ListNode? {
+    if (null == header?.next) return header
+    var pre: ListNode? = null
+    var cut: ListNode? = header
+    var next: ListNode? = null
+    while (null != cut) {
+        next = cut.next
+        cut.next = pre
+        pre = cut
+        cut = next
+    }
+    return pre
+}
--- sortalgorithmskt/src/main/java/cn/readsense/sortalgorithmskt/QuickSort_2020.07.16.kt
+++ sortalgorithmskt/src/main/java/cn/readsense/sortalgorithmskt/QuickSort_2020.07.16.kt
@@ -2,25 +2,23 @@ package cn.readsense.sortalgorithmskt
 
 /**
  *Author:qyg
- *DATE:2020/7/15 10:01
+ *DATE:2020/7/16 17:42
  *Description：
  **/
 
 fun main() {
-    val array = intArrayOf(86, 11, 77, 23, 32, 45, 58, 63, 93, 4, 37, 22)
-    sort(array, 0, array.size - 1)
-    array.forEach {
+    val intArray = intArrayOf(86, 11, 77, 23, 32, 45, 58, 63, 93, 4, 37, 22)
+    sort(intArray, 0, intArray.size - 1)
+    intArray.forEach {
         println(it)
     }
 }
 
 private fun sort(array: IntArray, start: Int, end: Int) {
-    if (array.isEmpty() || start < 0 || end >= array.size || end < start) {
-        return
-    }
+    if (array.isEmpty() || start < 0 || end >= array.size || start > end) return
     val partition = partition(array, start, end)
-    if (start < partition) sort(array, start, partition - 1)
-    if (end > partition) sort(array, partition + 1, end)
+    if (partition > start) sort(array, start, partition - 1)
+    if (partition < end) sort(array, partition + 1, end)
 }
 
 private fun partition(array: IntArray, start: Int, end: Int): Int {
